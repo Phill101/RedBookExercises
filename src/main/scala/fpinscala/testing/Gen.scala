48c28e87af89d0606d2134a94ff71fcf70d7294c
nsfilimonov <n.s.filimonov@tinkoff.ru>
2018-09-26 10:21:53 +0500

    some fun with 'testing library'

--- src/main/scala/fpinscala/testing/Gen.scala
+++ src/main/scala/fpinscala/testing/Gen.scala
@@ -6,7 +6,9 @@ import fpinscala.parallelism._
 import fpinscala.parallelism.Par.Par
 import Gen._
 import Prop._
-import java.util.concurrent.{Executors,ExecutorService}
+import java.util.concurrent.{ExecutorService, Executors}
+
+import fpinscala.state.RNG.Simple
 
 /*
 The library developed in this chapter goes through several iterations. This file is just the
@@ -21,12 +23,35 @@ object Prop {
 }
 
 object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+  def unit[A](a: => A): Gen[A] = Gen(State.unit(a))
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+    def generateFun: State[RNG, Int] = {
+      for {
+        q <- RNG.nonNegativeLessThan(stopExclusive)
+        r <- if (q >= start) State.unit[RNG, Int](q) else generateFun
+      } yield r
+    }
+
+    Gen(generateFun)
+  }
+
+  def boolean: Gen[Boolean] = Gen(RNG.nonNegativeLessThan(2).map(i => if (i > 0) true else false))
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = Gen(State.sequence(List.fill(n)(g.sample)))
+  def listOfN(size: Gen[Int]): Gen[List[Int]] = size.flatMap(a => listOfN(a, size) )
+
+  def tuple2[A](g: Gen[A]): Gen[(A, A)] =
+    Gen(for {
+      f <- g.sample
+      s <- g.sample
+    } yield (f, s))
+
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = choose(1, 3).flatMap(i => if (i < 2) g1 else g2)
 }
 
-trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+case class Gen[A](sample: State[RNG, A]) {
+  def map[B](f: A => B): Gen[B] = ???
+  def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(f.andThen(_.sample)))
 }
 
 trait SGen[+A] {
---
+++ src/main/scala/fpinscala/testing/ws.sc
@@ -0,0 +1,19 @@
+import fpinscala.state.RNG.Simple
+import fpinscala.testing.Gen
+
+val s = Simple(42)
+
+Gen.choose(5, 15).sample.run(s)
+
+Gen.boolean.sample.run(s)
+Gen.listOfN(5, Gen.boolean).sample.run(s)
+
+Gen.tuple2(Gen.choose(0, 10)).sample.run(s)
+
+Gen.listOfN(Gen.choose(1, 5)).sample.run(s)
+
+
+Gen.union(
+  Gen.choose(1, 5),
+  Gen.choose(100, 150)
+).sample.run(Simple(8))
